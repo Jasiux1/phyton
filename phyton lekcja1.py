2f38bb96fb12d99e035a3e9ac536034b682e20cd
Jan Maciejewski <u162_janmac_waw@technischools.com>
2024-09-27 15:52:47 +0200

    seks

---
+++ phyton lekcja1.py
@@ -0,0 +1,43 @@
+from turtle import*
+def sigma():
+    print("czesc")
+    print("podaj liczbe")
+    x=int(input())
+    if x <= 5:
+        print("źle")
+        pensize(3)
+        penup()      
+        goto(0, -100)
+        pendown()
+        circle(100)
+        color("red")
+        penup()
+        goto(-70, 70)
+        pendown()
+        goto(70,-70)
+        penup()
+        goto(-70, -70)
+        pendown()
+        goto(70,70)
+        
+    else:
+        print("dobrze")
+        pensize(3)
+        penup()      
+        goto(0, -100) 
+        pendown()     
+        circle(100)
+        color("green")
+        penup()
+        goto(-50, 0)  
+        pendown()
+        goto(-10, -40)  
+        goto(50, 40)
+        
+    
+
+        
+
+        
+
+sigma()
